c61cb14c2e17d958e69c2d10917a671e713234a8
Isuru-Eranda <chanodyapraveen@gmail.com>
2024-11-05 14:28:52 +0530

    Add a link to Index.html

--- javaScript/home.js
+++ javaScript/home.js
@@ -4,23 +4,39 @@ let navProfileMenu= document.getElementById("nav-profile-menu");
 let navCartIcon=document.getElementById("nav-cart-icon");
 let navcartList=document.getElementById("nav-cart-list");
 
-
-window.addEventListener("click",(e)=>{
-    if(e.target == navCartIcon){
-
-    }else{
-        navcartList.classList.remove("active");
-    }
-})
-
-
-navProfile.addEventListener("click",()=>{
+navProfile.addEventListener("click",(e)=>{
     navProfileMenu.classList.toggle("active");
-    navcartList.classList.toggle("active");
+    navcartList.classList.remove("active");
 });
 
-navCartIcon.addEventListener("click",()=>{
+navCartIcon.addEventListener("click",(e)=>{
     navcartList.classList.toggle("active");
     navProfileMenu.classList.remove("active");
 });
+
+
+let searchIcon=document.getElementById("search-icon");
+let searchBar=document.querySelector(".search-bar");
+
+searchIcon.addEventListener("click", function(){
+    searchBar.classList.toggle("active");
+});
+
+window.addEventListener("click",(e)=>{
+    if(!searchBar.contains(e.target) && !searchIcon.contains(e.target)){
+        searchBar.classList.remove("active")
+
+    }
+    if(!navProfileMenu.contains(e.target) && !navProfile.contains(e.target)) {
+        navProfileMenu.classList.remove("active");
+    }
+    if(navcartList.contains(e.target) && !navProfile.contains(e.target)){
+        navcartList.classList.remove("active");
+    }
+});
+
+
+
+
+
 
